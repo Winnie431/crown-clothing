4e43da47f06c53ddc4651189f9230b5cadb7ebe7
Winnie431 <80514918+Winnie431@users.noreply.github.com>
2022-12-31 23:22:09 +0100

    build sign-in ad sign-out componant containing the sign in-form component and the button component with their corresponding styles.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component.jsx";
+import SignInAndSignOutPage from "./pages/sign-in-and-sin-up/sign-in-and-sign-up.component";
+
 import Header from "./components/header/header.component.jsx";
 
 function App() {
@@ -15,7 +17,9 @@ function App() {
       <Routes>
         
           <Route exact path="/" element={<HomePage />} />
-          <Route exact path="/shop" element={<ShopPage />} />
+          <Route  path="/shop" element={<ShopPage />} />
+          <Route  path="/signin" element={<SignInAndSignOutPage />} />
+          
         
       </Routes>
     </div>
